e3207e9609d92a2780d2b3527e27c0e0f3626640
Otsuki Hitoshi <hitochan777@gmail.com>
2019-06-06 22:31:43 +0900

    HMR works

--- server/index.ts
+++ server/index.ts
@@ -1,20 +1,7 @@
 import { ApolloServer, gql } from "apollo-server";
 
-const typeDefs = gql`
-  type Book {
-    title: String
-    author: String
-  }
-  type Query {
-    books: [Book]
-  }
-`;
-
-const resolvers = {
-  Query: {
-    books: () => []
-  }
-};
+import typeDefs from "./typeDefs";
+import resolvers from "./resolvers";
 
 const server = new ApolloServer({ typeDefs, resolvers });
 
@@ -22,11 +9,12 @@ server.listen().then(({ url }) => {
   console.log(`🚀  Server ready at ${url}`);
 });
 
-// Here HMR story begins
 declare const module: any;
-// You need only 3 lines of code to start accepting code changes coming through the HMR
 if (module.hot) {
-  module.hot.accept();
-  // Next callback is essential: After code changes were accepted     we need to restart the app. server.close() is here Express.JS-specific and can differ in other frameworks. The idea is that you should shut down your app here. Data/state saving between shutdown and new start is possible
-  module.hot.dispose(async () => await server.stop());
+  module.hot.accept((error: Error) => {
+    console.error(error);
+  });
+  module.hot.dispose(async () => {
+    await server.stop();
+  });
 }

d7f208e834d8a79997fdda8a7755dd72bfcc2964
Otsuki Hitoshi <hitochan777@gmail.com>
2019-06-08 21:51:37 +0900

    wip

--- client/src/routes/index.svelte
+++ client/src/routes/index.svelte
@@ -32,6 +32,60 @@
 	}
 </style>
 
+<script>
+import firebase from "firebase/app";
+import "firebase/auth";
+
+
+let isLoggedIn = false;
+
+firebase.auth().onAuthStateChanged(function(user) {
+  if (user) {
+    isLoggedIn = true;
+    // User is signed in and currentUser will no longer return null.
+  } else {
+    // No user is signed in.
+  }
+});
+
+
+const handleSignOut = async () => {
+  try {
+    await firebase.auth().signOut();
+    isLoggedIn = false
+  } catch (error) {
+    console.error("Faild to sign out")
+  }
+}
+
+const provider = new firebase.auth.GoogleAuthProvider();
+const handleSignIn = async () => {
+  firebase.auth().signInWithRedirect(provider);
+  try {
+    const result = await firebase.auth().getRedirectResult()
+    if (result.credential) {
+      // This gives you a Google Access Token. You can use it to access the Google API.
+      const token = result.credential.accessToken;
+      console.log(token)
+      // ...
+    }
+    // The signed-in user info.
+    const user = result.user;
+  } catch (error) {
+    console.log(error)
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // The email of the user's account used.
+    var email = error.email;
+    // The firebase.auth.AuthCredential type that was used.
+    var credential = error.credential;
+  }
+}
+
+
+</script>
+
 <svelte:head>
 	<title>Sapper project template</title>
 </svelte:head>
@@ -44,3 +98,8 @@
 </figure>
 
 <p><strong>Try editing this file (src/routes/index.svelte) to test live reloading.</strong></p>
+{#if isLoggedIn}
+  <button on:click={handleSignOut}>Sign Out</button> 
+{:else}
+  <button on:click={handleSignIn}>Sign In With Google</button> 
+{/if}

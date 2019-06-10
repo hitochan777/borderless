681115380222bd22392421ce560a3fc843664004
Otsuki Hitoshi <hitochan777@gmail.com>
2019-06-10 21:46:17 +0900

    Format

--- client/src/client.js
+++ client/src/client.js
@@ -1,19 +1,6 @@
 import * as sapper from "@sapper/app";
 
-import firebase from "firebase/app";
-import "firebase/auth";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyBjiUl8vNuxrh284VEi-42PIX4bFtCBOxo",
-  authDomain: "polyglot-3f093.firebaseapp.com",
-  databaseURL: "https://polyglot-3f093.firebaseio.com",
-  projectId: "polyglot-3f093",
-  storageBucket: "polyglot-3f093.appspot.com",
-  messagingSenderId: "1045048195124",
-  appId: "1:1045048195124:web:41e1fe0602adcf69"
-};
-
-firebase.initializeApp(firebaseConfig);
+import "./firebase";
 
 sapper.start({
   target: document.querySelector("#sapper")
---
+++ client/src/firebase.js
@@ -0,0 +1,24 @@
+import firebase from "firebase/app";
+import "firebase/auth";
+import ky from "ky";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBjiUl8vNuxrh284VEi-42PIX4bFtCBOxo",
+  authDomain: "polyglot-3f093.firebaseapp.com",
+  databaseURL: "https://polyglot-3f093.firebaseio.com",
+  projectId: "polyglot-3f093",
+  storageBucket: "polyglot-3f093.appspot.com",
+  messagingSenderId: "1045048195124",
+  appId: "1:1045048195124:web:41e1fe0602adcf69"
+};
+
+if (firebase.apps.length === 0 && process.browser) {
+  firebase.initializeApp(firebaseConfig);
+  firebase.auth().onAuthStateChanged(async user => {
+    if (user) {
+      const token = await user.getIdToken();
+      // FIXME: const csrfToken = getCookie("csrfToken");
+      const parsed = await ky.post("/sessionLogin", { json: { token } }).json();
+    }
+  });
+}

5b035d8967d2dd510ecdc7cb36f08a6493c2a9fd
Otsuki Hitoshi <hitochan777@gmail.com>
2019-06-22 04:05:48 +0900

    Make dummy schema

--- server/index.ts
+++ server/index.ts
@@ -1,21 +1,10 @@
-import { ApolloServer, gql } from "apollo-server";
+import { ApolloServer, gql, addMockFunctionsToSchema } from "apollo-server";
 
-import typeDefs from "./typeDefs";
-import resolvers from "./resolvers";
 import { schema } from "./schema";
 
-const server = new ApolloServer(schema);
+addMockFunctionsToSchema({ schema });
+const server = new ApolloServer({ schema });
 
 server.listen().then(({ url }) => {
   console.log(`🚀  Server ready a ${url}`);
 });
-
-// declare const module: any;
-// if (module.hot) {
-//   module.hot.accept((error: Error) => {
-//     console.error(error);
-//   });
-//   module.hot.dispose(async () => {
-//     await server.stop();
-//   });
-// }

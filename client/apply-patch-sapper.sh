757891d4efdacb5614cdae2ff558b9287fe4f357
Otsuki Hitoshi <hitochan777@gmail.com>
2019-06-23 01:58:29 +0900

    Inplace replacement

--- client/apply-patch-sapper.sh
+++ client/apply-patch-sapper.sh
@@ -1,1 +1,1 @@
-sed 's/const session = session_getter(req, res);/const session = await session_getter(req, res);/' ./node_modules/sapper/runtime/server.mjs
+sed -i 's/const session = session_getter(req, res);/const session = await session_getter(req, res);/' ./node_modules/sapper/runtime/server.mjs

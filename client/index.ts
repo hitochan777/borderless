7e617ddc5c809f39ed5e506cf50ca93736dd6686
Otsuki Hitoshi <hitochan777@gmail.com>
2019-07-04 15:40:01 +0900

    Fix no content problem

--- client/index.ts
+++ client/index.ts
@@ -1,23 +1,16 @@
-import { router, get, post, ServerRequest, ServerResponse } from "microrouter";
+import {
+  router,
+  get,
+  post,
+  ServerRequest,
+  ServerResponse
+} from "microrouter";
 import next from "next";
 import httpProxy from "http-proxy";
 
 const dev = process.env.NODE_ENV !== "production";
 
-let globalRes: any;
-
 const proxy = httpProxy.createProxyServer();
-proxy.on("proxyRes", function(proxyRes, _, res) {
-  let body = new Buffer("");
-  proxyRes.on("data", function(data) {
-    body = Buffer.concat([body, data]);
-  });
-  proxyRes.on("end", function() {
-    const bodyString = body.toString();
-    console.log("res from proxied server:", bodyString);
-    res.end(bodyString);
-  });
-});
 
 const nextApp = next({ dev });
 const nextHandler = nextApp.getRequestHandler();
@@ -26,11 +19,10 @@ const GRAPHQL_PATH = "/graphql";
 const GRAPHQL_ENDPOINT = "http://localhost:3001";
 
 const graphqlProxyHandler = (req: ServerRequest, res: ServerResponse): any => {
-  globalRes = res;
   proxy.web(
     req,
     res,
-    { target: GRAPHQL_ENDPOINT, selfHandleResponse: true },
+    { target: GRAPHQL_ENDPOINT },
     err => {
       console.error(err);
     }
@@ -41,8 +33,8 @@ const createHandler = async () => {
   await nextApp.prepare();
 
   return router(
-    get(GRAPHQL_PATH, graphqlProxyHandler),
     post(GRAPHQL_PATH, graphqlProxyHandler),
+    get(GRAPHQL_PATH, graphqlProxyHandler),
     get("*", nextHandler)
   );
 };

e50625c8ebeeecb1d62a885cf3edbc9a24a47d59
Otsuki Hitoshi <hitochan777@gmail.com>
2020-05-07 00:25:32 +0900

    Use expansion panel

--- server/entity/correction_group.ts
+++ server/entity/correction_group.ts
@@ -19,6 +19,11 @@ export class CorrectionGroup extends Base {
     corrections: Tweet[],
     summaryComment: Tweet | null
   ): CorrectionGroup {
+    if (!summaryComment && corrections.length === 0) {
+      throw new Error(
+        "You need to provide at least one correction or summary comment"
+      );
+    }
     const userIds = corrections.map((correction) => correction.userId);
     if (new Set(userIds).size > 1) {
       throw new Error("Found more than one unique user ids");
@@ -27,18 +32,27 @@ export class CorrectionGroup extends Base {
     if (new Set(postIds).size > 1) {
       throw new Error("Found more than one unique post ids");
     }
-    const userId = userIds[0];
-    if (summaryComment && userId !== summaryComment.userId) {
-      throw new Error(
-        "Owner of summary comment is different from that of corrections"
-      );
+    const userId = userIds[0] || summaryComment?.userId;
+    if (!userId) {
+      throw new Error("User ID is not defined");
     }
-    const postId = postIds[0];
-    if (summaryComment && postId !== summaryComment.postId) {
-      throw new Error(
-        "PostId of summary comment is different from that of corrections"
-      );
+    const postId = postIds[0] || summaryComment?.postId;
+    if (!postId) {
+      throw new Error("Post ID is not defined");
     }
+    if (corrections.length > 0) {
+      if (summaryComment && userId !== summaryComment.userId) {
+        throw new Error(
+          "Owner of summary comment is different from that of corrections"
+        );
+      }
+      if (summaryComment && postId !== summaryComment.postId) {
+        throw new Error(
+          "PostId of summary comment is different from that of corrections"
+        );
+      }
+    }
+
     const correctionIds = corrections.map((correction) => correction.id);
     return new CorrectionGroup(
       null,

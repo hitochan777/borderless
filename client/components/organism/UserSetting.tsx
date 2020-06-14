f98b343343f42e2f6f125ac0bb67472adf19891e
Otsuki Hitoshi <hitochan777@gmail.com>
2020-06-14 12:19:01 +0900

    Delete unnecesary line

--- client/components/organism/UserSetting.tsx
+++ client/components/organism/UserSetting.tsx
@@ -52,7 +52,6 @@ export const UserSetting: React.FC = () => {
       i18n.i18n.changeLanguage(values.language);
     },
   });
-  console.log(viewer, formik.values);
 
   if (viewerQueryLoading || languageQueryLoading || timezoneQueryLoading) {
     return <div>loading...</div>;
